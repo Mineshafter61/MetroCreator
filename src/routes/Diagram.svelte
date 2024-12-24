644fb6711fd88df145098bcb55bd73ef21227683
Mike Shafter <yuhang.061@gmail.com>
2024-12-24 19:56:48 +0800

    Add 90˚ curves and straight line (horizontal, vertical and diagonal) makers

--- src/routes/Diagram.svelte
+++ src/routes/Diagram.svelte
@@ -1,6 +1,55 @@
 <script>
+// Constants
+// - Curves
+const curve = {
+	SW: "q 0  20,-20  20",
+	SE: "q 0  20, 20  20",
+	NW: "q 0 -20,-20 -20",
+	NE: "q 0 -20, 20 -20",
+	WN: "q -20 0,-20 -20",
+	WS: "q -20 0,-20  20",
+	EN: "q  20 0, 20 -20",
+	ES: "q  20 0, 20  20"
+};
+
+const dir = {
+	N : 1,
+	S : 2,
+	E : 4,
+	W : 8,
+	NE: 1 | 4,
+	SE: 2 | 4,
+	NW: 1 | 8,
+	SW: 2 | 8
+};
+
+// Adjusted makeLine function
+const makeLine = (/** @type {number} */ length, /** @type {number} */ direction) => {
+	switch (direction) {
+		case dir.N:
+			return `v -${length}`;
+		case dir.S:
+			return `v ${length}`;
+		case dir.E:
+			return `h ${length}`;
+		case dir.W:
+			return `h -${length}`;
+		case dir.NE:
+			return `l ${length} -${length}`;
+		case dir.SE:
+			return `l ${length} ${length}`;
+		case dir.NW:
+			return `l -${length} -${length}`;
+		case dir.SW:
+			return `l -${length} ${length}`;
+		default:
+			return ``;
+	}
+};
+
 let color = "red";
-let path = "M250,250 h250 q 20 0, 20 20 v250";
+
+let path = ["M500,500", makeLine(100, dir.E), curve.EN, curve.NE, curve.ES, makeLine(100, dir.S), curve.SW, curve.WN, curve.NE, makeLine(100, dir.E)].join(" ");
 </script>
 
 <h1>Canvas Test</h1>
